bb1e961f1b42965014faca93089d3a4b0ea26ee7
Cezary Bober <ceski23@gmail.com>
2019-04-09 22:36:12 +0200

    part 2

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ from random import randrange
 np.set_printoptions(formatter={'float': '{: 0.2f}'.format})
 # np.seterr('raise')
 
+def load_zoo(test_count):
+    with open('zoo.csv') as f:
+        animals = np.array([list(map(int, x.strip().split(',')[1:])) for x in f])
+        np.random.shuffle(animals)
+        a = int((animals.shape[0] / 100) * test_count)
+
+        test = animals[:a]
+        test = test[test[:,16].argsort()]
+        test_data = (normalize(test[:, :16].T).T, test[:, 16])
+
+        learning = animals[a:]
+        learning = learning[learning[:,16].argsort()]
+        learning_data = (normalize(learning[:, :16].T).T, learning[:, 16])
+    return [learning_data, test_data]
+
 def load_wine(test_count):
     with open('wine.csv') as f:
         wines = np.array([list(map(float, x.strip().split(','))) for x in f])
@@ -32,24 +47,24 @@ def normalize(data, min_v=0, max_v=1):
 if __name__ == "__main__":
     learning_data, testing_data = load_wine(test_count=20)
     
-    net = nn.NeuralNetwork(0.2, 20, [26, 12, 4], 1.04, 1.05, 0.7, 0.020)
-    net.feed_training_data(*learning_data)
-    net.feed_test_data(*testing_data)
-    net.start_learning(live_plot=False)
-    net.save_model()
+    # net = nn.NeuralNetwork(0.05, 100, [50], 1.04, 1.05, 0.7, 0.020)
+    net = nn.NeuralNetwork(0.1, 5000, [16, 12], 1.04, 1.05, 0.7, 0.020)
+    # net.feed_training_data(*learning_data)
+    # net.feed_test_data(*testing_data)
+    # net.start_learning(live_plot=True)
+    # net.save_model()
+    net.load_model('models/091876_16_12.mdl')
 
 
     plt.figure()
     for i in range(4):
         P, T = load_wine(test_count=20)[1]
-        prediction = [net.predict(x) for x in P]
-        error = [d-y for y, d in zip(prediction, T)]
-        cost = [0.5 * (e**2) for e in error]
+        prediction, cost = net.test(P, T)
 
         plt.subplot(2, 2, i+1)
         plt.grid(linestyle='--')
         plt.yticks(np.arange(min(T), max(T)+0.01, 0.25))
         plt.plot(prediction)
         plt.plot(T)
-        plt.title(f'MAX cost: {max(cost):.8f}')
+        plt.title(f'MAX cost: {cost:.8f}')
     plt.show()
--- neural_network.py
+++ neural_network.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from time import time
 import pickle
 import math
 
@@ -39,28 +38,38 @@ class NeuralNetwork:
         sizes = [self.P.shape[1], *self.layers, 1]
         for i in range(1, len(sizes)):
             self.weights.append(np.random.rand(sizes[i], sizes[i-1]))
+            # self.weights.append(np.ones((sizes[i], sizes[i-1])))
 
     def predict(self, x):
-        y = [x]
-        for i in range(len(self.weights)):
-            f = self.linear if i == len(self.weights) - 1 else self.sigmoid
-            s = np.array([self.forward(y[i], w) for w in self.weights[i]])
+        return self.forward(x)[0][-1][0]
+##################################################################################################################################
+    def forward(self, x):
+        '''Oblicza sygnał wyjściowy neuronów'''
+        y, ss = [x], []
+        for i in range(len(self.layers) + 1):
+            f = self.linear if i == len(self.layers) else self.sigmoid
+            s = np.array([sum(y[i] * w) for w in self.weights[i]])      # w - wagi jednego neuronu
             y.append(f(s))
-        return y[-1][0]
+            ss.append(s)
+        return y, ss
 
-    def forward(self, x, w):
-        '''Oblicza wyjście dla pojedynczego neuronu'''
-        return sum(x * w)
-
-    def neuron_error(self, l, w):
-        '''Propagacja błędu w jednej warstwie'''
-        return sum(l * w)
-
-    def update_weights(self, x, w, func, l, e):
-        '''Obliczanie wag dla pojedynczego neuronu'''
-        d = self.lr * l * func(e, True) * x
-        return w + d
+    def errors(self, d, y, ss):
+        delta = [d - y[-1]]
+        for k in range(len(self.layers), 0, -1):
+            epsilon = [sum(delta[0] * w) for w in self.weights[k].T]    # w - wagi jednego neuronu
+            delta.insert(0, np.array(epsilon * self.sigmoid(ss[k-1], True)))
+        return delta
     
+    def test(self, P, T):
+        prediction = [self.predict(x) for x in P]
+        error = [d - y for y, d in zip(prediction, T)]
+        cost = max([(e**2) for e in error])
+        return prediction, cost
+
+    def update_weights(self, w, d, x):
+        '''Obliczanie wag dla pojedynczego neuronu'''
+        return w + (2 * self.lr * d * x)
+##################################################################################################################################
   
     def save_model(self, prefix='', weights=None):
         name = f'{self.costs[-1]:.5f}_{"_".join(map(str, self.layers))}'.replace(".", "")
@@ -68,12 +77,12 @@ class NeuralNetwork:
         weights = self.weights if weights is None else weights
         
         with open(f'models/{name}.mdl', 'wb') as f:
-            pickle.dump(self.weights, f)
+            pickle.dump((self.weights, self.layers), f)
             print(f'Zapisano model jako: {name}.mdl')
     
     def load_model(self, path):
         with open(path, 'rb') as f:
-            self.weights = pickle.load(f)  
+            self.weights, self.layers = pickle.load(f)
     
     def start_learning(self, live_plot=False, plot_interval=1):
         if live_plot:
@@ -89,32 +98,23 @@ class NeuralNetwork:
 
         last_weights = []
         backup = {'weights': None, 'cost': math.inf, 'epoch': -1}
-
+##################################################################################################################################
         for epoch in range(self.epochs):
             for x, d in zip(self.P, self.T):
-                y, ss = [x], []
+                y, ss = self.forward(x)
+                delta = self.errors(d, y, ss)
 
-                for i in range(len(self.weights)):
-                    f = self.linear if i == len(self.weights) - 1 else self.sigmoid
-                    s = np.array([self.forward(y[i], w) for w in self.weights[i]])
-                    y.append(f(s))
-                    ss.append(s)
-
-                errors = [d - y[-1]]
-                for layer_idx in range(len(self.weights)-1, 0, -1):
-                    layer_error = [self.neuron_error(errors[0], w) for w in self.weights[layer_idx].T]
-                    errors.insert(0, layer_error)
-
-                for i in range(len(self.weights)):
-                    func = self.linear if i == len(self.weights) - 1 else self.sigmoid
+                for i in range(len(self.layers) + 1):
                     for j in range(len(self.weights[i])):
-                        self.weights[i][j] = self.update_weights(y[i], self.weights[i][j], func, errors[i][j], ss[i][j])
+                        self.weights[i][j] = self.update_weights(self.weights[i][j], delta[i][j], y[i])
+##################################################################################################################################
 
-
-            prediction = [self.predict(x) for x in self.test_P]
-            error = [d - y for y, d in zip(prediction, self.test_T)]
-            cost = max([0.5 * (e**2) for e in error])
+            # prediction = [self.predict(x) for x in self.test_P]
+            # error = [d - y for y, d in zip(prediction, self.test_T)]
+            # cost = max([(e**2) for e in error])
+            prediction, cost = self.test(self.test_P, self.test_T)
             self.costs.append(cost)
+##################################################################################################################################
 
             if cost <= self.goal:
                 print(f'Achieved goal with cost: {cost}')
@@ -129,10 +129,10 @@ class NeuralNetwork:
 
             last_weights = self.weights
 
-            if cost < backup['cost']:
-                backup['cost'] = cost
-                backup['weights'] = self.weights
-                backup['epoch'] = epoch
+            # if cost < backup['cost']:
+            #     backup['cost'] = cost
+            #     backup['weights'] = self.weights
+            #     backup['epoch'] = epoch
 
             if live_plot and not epoch % plot_interval:
                 line.set_ydata(prediction)
@@ -151,5 +151,5 @@ class NeuralNetwork:
         plt.plot(self.costs)
         plt.show()
 
-        if backup['cost'] != cost:
-            self.save_model(f'BCP_{backup["epoch"]}_', backup['weights'])
+        # if backup['cost'] != cost:
+        #     self.save_model(f'BCP_{backup["epoch"]}_', backup['weights'])
